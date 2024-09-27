81841bba1e0951be5b0fee9ababa0b5f5d0b6318
ChrisClark303 <38655477+ChrisClark303@users.noreply.github.com>
2024-09-27 07:44:32 +0100

    Converted if to foreach to remove the use of the collection indexer everywhere

--- GildedRose/GildedRose.cs
+++ GildedRose/GildedRose.cs
@@ -12,75 +12,75 @@ namespace GildedRoseKata
 
         public void UpdateQuality()
         {
-            for (var i = 0; i < Items.Count; i++) //for instead of foreach! 
+            foreach (Item item in Items) //for instead of foreach! 
             {
                 //nested ifs! Magic strings! 
-                if (Items[i].Name != "Aged Brie" && Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
+                if (item.Name != "Aged Brie" && item.Name != "Backstage passes to a TAFKAL80ETC concert")
                 {
-                    if (Items[i].Quality > 0)
+                    if (item.Quality > 0)
                     {
-                        if (Items[i].Name != "Sulfuras, Hand of Ragnaros") //can probably combine this with the above IF?
+                        if (item.Name != "Sulfuras, Hand of Ragnaros") //can probably combine this with the above IF?
                         {
-                            Items[i].Quality--;
+                            item.Quality--;
                         }
                     }
                 }
                 else
                 {
-                    if (Items[i].Quality < 50) //magic number
+                    if (item.Quality < 50) //magic number
                     {
-                        Items[i].Quality++;
+                        item.Quality++;
 
-                        if (Items[i].Name == "Backstage passes to a TAFKAL80ETC concert") //magic string
+                        if (item.Name == "Backstage passes to a TAFKAL80ETC concert") //magic string
                         {
-                            if (Items[i].SellIn < 11) //magic number
+                            if (item.SellIn < 11) //magic number
                             {
-                                if (Items[i].Quality < 50) //magic number
+                                if (item.Quality < 50) //magic number
                                 {
-                                    Items[i].Quality++;
+                                    item.Quality++;
                                 }
                             }
 
-                            if (Items[i].SellIn < 6) //magic number
+                            if (item.SellIn < 6) //magic number
                             {
-                                if (Items[i].Quality < 50) //magic number
+                                if (item.Quality < 50) //magic number
                                 {
-                                    Items[i].Quality++;
+                                    item.Quality++;
                                 }
                             }
                         }
                     }
                 }
 
-                if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
+                if (item.Name != "Sulfuras, Hand of Ragnaros")
                 {
-                    Items[i].SellIn--;
+                    item.SellIn--;
                 }
 
-                if (Items[i].SellIn < 0) //HandlePastSellBy()? - could return early instead of all these nested ifs! 
+                if (item.SellIn < 0) //HandlePastSellBy()? - could return early instead of all these nested ifs! 
                 {
-                    if (Items[i].Name != "Aged Brie") //magic string
+                    if (item.Name != "Aged Brie") //magic string
                     {
-                        if (Items[i].Name != "Backstage passes to a TAFKAL80ETC concert") //magic string
+                        if (item.Name != "Backstage passes to a TAFKAL80ETC concert") //magic string
                         {
-                            if (Items[i].Quality > 0) //combine ifs
+                            if (item.Quality > 0) //combine ifs
                             {
-                                if (Items[i].Name != "Sulfuras, Hand of Ragnaros") //magic string
+                                if (item.Name != "Sulfuras, Hand of Ragnaros") //magic string
                                 {
-                                    Items[i].Quality--;
+                                    item.Quality--;
                                 }
                             }
                         }
                         else
                         {
-                            Items[i].Quality = 0;
+                            item.Quality = 0;
                         }
                     }
                     else
                     {
-                        if (Items[i].Quality < 50) //magic number
+                        if (item.Quality < 50) //magic number
                         {
-                            Items[i].Quality++;
+                            item.Quality++;
                         }
                     }
                 }
