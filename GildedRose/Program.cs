83b8716cf3f4da9031fae00142f8a4d84774e0fe
ChrisClark303 <38655477+ChrisClark303@users.noreply.github.com>
2024-09-29 12:00:05 +0100

    Replaced hardcoded item names with references to ItemNames class, to ensure everything is referring to the same underlying string value

--- GildedRose/Program.cs
+++ GildedRose/Program.cs
@@ -14,31 +14,31 @@ namespace GildedRoseKata
             Console.WriteLine("OMGHAI!");
 
             IList<Item> Items = new List<Item>{
-                new Item {Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20},
-                new Item {Name = "Aged Brie", SellIn = 2, Quality = 0},
-                new Item {Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7},
-                new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80},
-                new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = -1, Quality = 80},
+                new Item {Name = ItemNames.DexterityVest, SellIn = 10, Quality = 20},
+                new Item {Name = ItemNames.AgedBrie, SellIn = 2, Quality = 0},
+                new Item {Name = ItemNames.MongooseElixir, SellIn = 5, Quality = 7},
+                new Item {Name = ItemNames.Sulfuras, SellIn = 0, Quality = 80},
+                new Item {Name = ItemNames.Sulfuras, SellIn = -1, Quality = 80},
                 new Item
                 {
-                    Name = "Backstage passes to a TAFKAL80ETC concert",
+                    Name = ItemNames.BackstagePass,
                     SellIn = 15,
                     Quality = 20
                 },
                 new Item
                 {
-                    Name = "Backstage passes to a TAFKAL80ETC concert",
+                    Name = ItemNames.BackstagePass,
                     SellIn = 10,
                     Quality = 49
                 },
                 new Item
                 {
-                    Name = "Backstage passes to a TAFKAL80ETC concert",
+                    Name = ItemNames.BackstagePass,
                     SellIn = 5,
                     Quality = 49
                 },
 				// this conjured item does not work properly yet
-				new Item {Name = "Conjured Mana Cake", SellIn = 3, Quality = 6}
+				new Item {Name = ItemNames.ConjuredManaCake, SellIn = 3, Quality = 6}
             };
 
             var app = new GildedRose(Items);
