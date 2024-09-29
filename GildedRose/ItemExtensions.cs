9e910458272130bd170202c765f7ae4f9e2bc24b
ChrisClark303 <38655477+ChrisClark303@users.noreply.github.com>
2024-09-29 11:29:32 +0100

    Moved the logic to set the extra quality updates based on the value of SellIn into the ItemExtensions. This seems to be where a lot of the rule logic is ending up, which I'm on the fence about

--- GildedRose/ItemExtensions.cs
+++ GildedRose/ItemExtensions.cs
@@ -1,9 +1,13 @@
-﻿namespace GildedRoseKata
+﻿using System;
+using System.Linq;
+
+namespace GildedRoseKata
 {
     public static class ItemExtensions
     {
         private const int MaxQuality = 50;
         private const int MinQuality = 0;
+        private static readonly int[] ItemSellInThresholds = [10, 5];
 
         public static void DecrementQualityIfNotAtMin(this Item item)
         {
@@ -44,6 +48,16 @@
         public static bool HasSellInDependentQualityUpdates(this Item item)
         {
             return item.Name == ItemNames.BackstagePass;
+        }
+
+        public static void ApplySellInDependentQualityUpdate(this Item item)
+        {
+            if (item.HasSellInDependentQualityUpdates())
+            {
+                int qualityUpdate = ItemSellInThresholds.Where(t => item.SellIn <= t)
+                    .Count();
+                item.Quality = Math.Clamp(item.Quality + qualityUpdate, MinQuality, MaxQuality);
+            }
         }
     }
 }
--- GildedRoseTests/ItemExtensionTests.cs
+++ GildedRoseTests/ItemExtensionTests.cs
@@ -115,5 +115,32 @@ namespace GildedRoseTests
 
             Assert.Equal(expected, actual);
         }
+
+        [Theory]
+        [InlineData("Aged Brie", 5, 0)]
+        [InlineData("Backstage passes to a TAFKAL80ETC concert", 11, 0)]
+        [InlineData("Backstage passes to a TAFKAL80ETC concert", 10, 1)]
+        [InlineData("Backstage passes to a TAFKAL80ETC concert", 5, 2)]
+        [InlineData("+5 Dexterity Vest", 5, 0)]
+        [InlineData("ItemX", 5, 0)]
+        [InlineData("Sulfuras, Hand of Ragnaros", 5, 0)]
+        public void ApplySellInDependentQualityUpdates_ReturnsCorrectBoolean_BasedOnItemName(string itemName, int itemSellIn, int qualityAfterUpdate)
+        {
+            Item item = new() { Name = itemName, SellIn = itemSellIn, Quality = 0 };
+
+            item.ApplySellInDependentQualityUpdate();
+
+            Assert.Equal(qualityAfterUpdate, item.Quality);
+        }
+
+        [Fact]
+        public void ApplySellInDependentQualityUpdates_DoesNotApplyQualityUpdate_PastMaxQuality()
+        {
+            Item item = new() { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 3, Quality = 49 };
+
+            item.ApplySellInDependentQualityUpdate();
+
+            Assert.Equal(50, item.Quality);
+        }
     }
 }
